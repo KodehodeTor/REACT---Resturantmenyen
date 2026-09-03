035c6829650a789f31b3f6a179c4ba093d573089
KodehodeTor <torjarand.26pg@gmail.com>
2026-09-03 09:15:12 +0200

    Imported meny and MenuItem to app.cs, amended typo in meny map function

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,27 @@
 import "./App.css";
+import { meny } from "./assets/data";
+import { MenuItem } from "./components/MenuItem";
 
 function App() {
   return (
     <div className="resturant_app">
       <h1>Vår Meny</h1>
       <div className="meny_list">
-
         {meny.map(function (item) {
-    return (
-      <MenuItem
-        key={item.id}
-        tittel={item.tittel}
-        pris={item.pris}
-        ingredienser={item.ingredienser}
-        kategori={item.kategori}
-      />
-    );
-  });
-}
+          return (
+            <MenuItem
+              key={item.id}
+              tittel={item.tittel}
+              pris={item.pris}
+              ingredienser={item.ingredienser}
+              kategori={item.kategori}
+            />
+          );
+        })}
+        ;
       </div>
     </div>
-    
-  )
+  );
 }
 
 export default App;
