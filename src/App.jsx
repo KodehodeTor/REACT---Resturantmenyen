71f6861b6a005478ac6f007da193502930ad72ad
KodehodeTor <torjarand.26pg@gmail.com>
2026-09-03 08:47:15 +0200

    Set up basic MenuItem function in app.jsx.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import "./App.css";
 import { meny } from "./assets/data.js";
 
-function App() {
-  return <></>;
+function MenuItem() {
+  return (
+    <div>
+      <h2>Spahetti Bolognes</h2>
+      <p>159 kr</p>
+      <p>Spagetthi, kjøttsaus, parmesan</p>
+      <p>Hovedrett</p>
+    </div>
+  );
 }
 
-export default App;
+export default MenuItem;
