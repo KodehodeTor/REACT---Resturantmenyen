bbd7c426277ecb2926ab790a05097f4753f931d4
KodehodeTor <torjarand.26pg@gmail.com>
2026-09-07 09:09:58 +0200

    Added basic styling to .menu_container

--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -1,6 +1,6 @@
 export function MenuItem({ tittel, pris, ingredienser, kategori }) {
   return (
-    <div className="Menu_Container">
+    <div className="menu_container">
       <h2>{tittel}</h2>
       <p>{pris}</p>
       <p>{ingredienser}</p>
