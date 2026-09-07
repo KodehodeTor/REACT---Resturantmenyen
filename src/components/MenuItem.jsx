6845e7b89d0d475953947c24ac5a06da5083388e
KodehodeTor <torjarand.26pg@gmail.com>
2026-09-07 09:00:30 +0200

    Emptied App.CSS / Index.CSS for a clean start for styling

--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -1,6 +1,6 @@
 export function MenuItem({ tittel, pris, ingredienser, kategori }) {
   return (
-    <div>
+    <div className="Menu_Container">
       <h2>{tittel}</h2>
       <p>{pris}</p>
       <p>{ingredienser}</p>
